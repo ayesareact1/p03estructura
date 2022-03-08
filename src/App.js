fa950f065bad4cc5c0a89085ad60c5089349b75e
ayesareact1 <ayesareact1@gmail.com>
2022-03-08 16:13:25 +0100

    jsx

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
+import logo from './img/logo.svg'; // Los estáticos deben ser importados
 import './App.css';
 
 function App() {
+
+  const mensaje = '¡Hola Mundo!';
+  const urlPic = 'https://i.ibb.co/HH9yRts/2.jpg';
+
+  const footer = <footer><p>&copy; 2022 ACME</p></footer>;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <h1>{mensaje}</h1>
+      <img className='logo' src={logo} />
+      <img className='logo' src={urlPic} />
+      {footer}
     </div>
   );
 }
